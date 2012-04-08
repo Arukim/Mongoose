159e62dfcdded8547adf71ec132d62db81855e61
Smelov Nikita <smelovn@gmail.com>
2012-04-08 23:26:24 +0400

    Added simple GUI

--- Source/Mongoose.py
+++ Source/Mongoose.py
@@ -15,13 +15,15 @@ class SerialManager:
 
 
 class MainApp:
+    ser = serial.Serial()
     # constructor, parent widget is passed to constructor
     def __init__(self, master):
         # container for next 2 widgets
         frame = Frame(master)
         frame.grid()
+        self.frame = frame
         # fg = foreground
-        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button = Button(frame, text="QUIT", fg="red", command=self.quit)
         self.button.grid(row=0)
 
         self.hi_there = Button(frame, text="Hello", command=self.say_hi)
@@ -31,26 +33,27 @@ class MainApp:
 
         self.textBox = Entry(master,text = "Hello")
         self.textBox.grid(row=1,sticky = W)
+
+        
+        self.ser.port = "COM9"
+        self.ser.baudrate = int(9600)
+        self.ser.timeout = 1
+
+        try:
+            self.ser.open()
+        except serial.SerialException as e:
+            logging.error("Could not open serial port %s: %s" % (self.ser.portstr, e))
+            sys.exit(1)
+        logging.info("Serving serial port: %s" % (self.ser.portstr,))
+        
+        #ser.close()
+
     def say_hi(self):
-        print("Hello world!")
+        self.ser.write(str.encode("Hello world\r\n",'ascii'))
+    def quit(self):
+        self.frame.quit()
+
+
 root = Tk()
-ser = serial.Serial()
-ser.port = "COM9"
-ser.baudrate = int(9600)
-ser.timeout = 1
-
-try:
-    ser.open()
-except serial.SerialException as e:
-    logging.error("Could not open serial port %s: %s" % (ser.portstr, e))
-    sys.exit(1)
-logging.info("Serving serial port: %s" % (ser.portstr,))
-output = "Hello world!"
-ser.write(str.encode("Hello world\r\n",'ascii'))
-#ser.flush()
-#ser.write(0(0x3x30)
-#ser.write(0x30)
-ser.close()
-#app = MainApp(root)
-
-#root.mainloop()
+app = MainApp(root)
+root.mainloop()
